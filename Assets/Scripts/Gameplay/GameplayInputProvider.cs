1284048a72c14aa128ea9d6915174e929fde7534
Mforce22 <matte.guardi@gmail.com>
2023-04-26 14:07:10 +0200

    Added Movement

--- Assets/Scripts/Gameplay/GameplayInputProvider.cs
+++ Assets/Scripts/Gameplay/GameplayInputProvider.cs
@@ -7,6 +7,7 @@ public class GameplayInputProvider : InputProvider
 {
     #region Delegate
     public OnFloatDelegate OnMove;
+    public OnFloatDelegate OnZMovePositive;
     public OnVoidDelegate OnJump;
     #endregion
 
@@ -15,24 +16,32 @@ public class GameplayInputProvider : InputProvider
     private InputActionReference _Move;
 
     [SerializeField]
+    private InputActionReference _ZMovePositive;
+
+    [SerializeField]
     private InputActionReference _Jump;
 
     private void OnEnable()
     {
         _Move.action.Enable();
         _Jump.action.Enable();
+        _ZMovePositive.action.Enable();
+
 
         _Move.action.performed += MovePerfomed;
         _Jump.action.performed += JumpPerfomed;
+        _ZMovePositive.action.performed += ZMovePositivePerfomed;
     }
 
     private void OnDisable()
     {
         _Move.action.Disable();
         _Jump.action.Disable();
+        _ZMovePositive.action.Disable();
 
         _Move.action.performed -= MovePerfomed;
         _Jump.action.performed -= JumpPerfomed;
+        _ZMovePositive.action.performed -= ZMovePositivePerfomed;
     }
 
     private void MovePerfomed(InputAction.CallbackContext obj)
@@ -44,5 +53,12 @@ public class GameplayInputProvider : InputProvider
     private void JumpPerfomed(InputAction.CallbackContext obj)
     {
         OnJump?.Invoke();
+    }
+
+    private void ZMovePositivePerfomed(InputAction.CallbackContext obj)
+    {
+        float value = obj.action.ReadValue<float>();
+        //Debug.LogFormat("ZMovePositivePerfomed: {0}", value);
+        OnZMovePositive?.Invoke(value);
     }
 }
--- Assets/Scripts/Player/CharacterController.cs
+++ Assets/Scripts/Player/CharacterController.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    [SerializeField]
-    private IdContainer _IdProvider;
+    [Header("Character Controller")]
+    [SerializeField] private IdContainer _IdProvider;
 
+
+    [Header("Character Movement")]
+    [SerializeField] private float _speed = 400f;
 
     private GameplayInputProvider _gameplayInputProvider;
+    private Vector3 _moveDirection = Vector3.zero;
 
     private void Awake()
     {
@@ -20,11 +24,18 @@ public class CharacterController : MonoBehaviour
     {
         _gameplayInputProvider.OnMove += MoveCharacter;
         _gameplayInputProvider.OnJump += JumpCharacter;
+        _gameplayInputProvider.OnZMovePositive += ZMove;
     }
     private void OnDisable()
     {
         _gameplayInputProvider.OnMove -= MoveCharacter;
         _gameplayInputProvider.OnJump -= JumpCharacter;
+        _gameplayInputProvider.OnZMovePositive -= ZMove;
+    }
+
+    private void Update()
+    {
+        transform.Translate(_moveDirection * _speed * Time.deltaTime);
     }
 
     private void JumpCharacter()
@@ -34,7 +45,18 @@ public class CharacterController : MonoBehaviour
 
     private void MoveCharacter(float value)
     {
+        //move the character
+        // Vector3 dir = new Vector3(value, 0, 0);
+        _moveDirection = new Vector3(value, 0, _moveDirection.z);
+        // transform.Translate(dir * _speed * Time.deltaTime);
         Debug.LogFormat("Value: {0}", value);
     }
+
+    private void ZMove(float value)
+    {
+        _moveDirection = new Vector3(_moveDirection.x, 0, value);
+        Debug.Log("ZMove " + value);
+    }
+
 
 }
