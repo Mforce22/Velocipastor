a11323b4847316ecd663f7d13f8e6a2b3613e183
Mforce22 <matte.guardi@gmail.com>
2023-05-02 15:46:06 +0200

    Added enemy death

--- Assets/Scripts/Enemies/EnemyCollisionHandler.cs
+++ Assets/Scripts/Enemies/EnemyCollisionHandler.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class EnemyCollisionHandler : MonoBehaviour
 {
+    [Header("Enemy Particle System")]
+    [SerializeField] private ParticleSystem _particleSystem;
+
+    [Header("Enemy Mesh")]
+    [SerializeField] private SkinnedMeshRenderer _meshRenderer;
+
+    private bool dead = false;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player" && !dead)
         {
+            dead = true;
             Debug.Log("Enemy Collision");
+            _meshRenderer.enabled = false;
+            _particleSystem.Play();
+            StartCoroutine(DestroyEnemy());
             //PlayerController.Instance.GetInput<GameplayInputProvider>(0).OnUsedPower?.Invoke();
         }
+    }
+
+    private IEnumerator DestroyEnemy()
+    {
+        yield return new WaitForSeconds(2f);
+        Destroy(gameObject);
     }
 
 }
